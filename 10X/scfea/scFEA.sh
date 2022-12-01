225f76690164c1b44b04a1782a3d0bfe763c6b5f
pmb59 <pmb59@users.noreply.github.com>
2022-12-01 22:44:34 +0000

    [auto] ci: apply beautysh changes

--- 10X/scfea/scFEA.sh
+++ 10X/scfea/scFEA.sh
@@ -10,6 +10,6 @@ conda install pytorch torchvision -c pytorch
 pip install --user magic-impute
 
 python src/scFEA.py --input_dir input --res_dir output \
-  --test_file counts.csv --moduleGene_file module_gene_complete_mouse_m168.csv \
-  --sc_imputation True
-  
+    --test_file counts.csv --moduleGene_file module_gene_complete_mouse_m168.csv \
+    --sc_imputation True
+
